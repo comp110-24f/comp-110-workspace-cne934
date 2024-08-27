c9e6fc80e645d7322f93db1faa48e1ac930acd28
Claire Evans <cnev@unc.edu>
2024-08-27 13:50:32 -0400

    backing up ex00 hello world

---
+++ lesson/inclass8_26.py
@@ -0,0 +1,11 @@
+""""practice with functions"""
+
+#define a function
+def sum(num1: int, num2:int) -> int:
+    """returns sum of num1 and num2"""
+    return num1 + num2
+
+
+#call the funtion 
+
+print(sum(num1=1, num2=10)) #<-- 1 and 10 are arguments                                        
